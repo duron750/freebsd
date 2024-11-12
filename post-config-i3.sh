b169c63a820ff9312f7f0b0df13a838baabf50c6
Daniel T <dan.tiganas@gmail.com>
2024-11-13 00:14:22 +0200

    cleaning up

--- post-config-i3.sh
+++ post-config-i3.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-pkg install xorg
+pkg install sudo neofetch htop zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
 pkg install i3 i3status i3lock dmenu
 pkg install utouch-kmod xf86-input-evdev
 pkg install rofi xfce4-terminal nitrogen gnome-screenshot qemu-guest-agent mate-backgrounds pavucontrol volumeicon
@@ -21,10 +21,14 @@ mkdir -p /home/dan/.config/i3
 mkdir -p /root/.config/i3
 cp i3/config /home/dan/.config/i3
 cp i3/config /root/.config/i3
+cp zshrc /home/dan/.zshrc
 chown -R dan /home/dan/.config
 chgrp -R dan /home/dan/.config
 chown dan /home/dan/.xinitrc
 chgrp dan /home/dan/.xinitrc
+chown dan /home/dan/.zshrc
+chgrp dan /home/dan/.zshrc
+echo "%wheel ALL=(ALL:ALL) ALL" >> /usr/local/etc/sudoers
 echo ""
 echo "Please reboot computer, then login as user, startx and run nitrogen to setup wallpaper" 
 echo "Select preferences and add folder /usr/local/share/backgrounds/mate"
