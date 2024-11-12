a25d7bb4b4417a8aa3748e74ae71c5cdcbac8d25
Daniel T <dan.tiganas@gmail.com>
2024-11-12 12:20:15 +0200

    added mouse driver for X

--- post-config-i3.sh
+++ post-config-i3.sh
@@ -2,10 +2,16 @@
 
 pkg install xorg
 pkg install i3 i3status i3lock dmenu
+pkg install utouch-kmod xf86-input-evdev
+
 
 echo "Configuring startup scripts"
 echo "dbus_enable=\"YES\"" >> /etc/rc.conf
 echo "hald_enable=\"YES\"" >> /etc/rc.conf
+
+echo "Enabling mouse in Qemu VirtualMachine"
+echo 'utouch_load="YES"' >> /boot/loader.conf
+echo ""
 
 echo "configuring local variables"
 echo "exec /usr/local/bin/i3" > /home/dan/.xinitrc
@@ -18,5 +24,5 @@ chown dan -R /home/dan/.config
 chgrp dan -R /home/dan/.config
 chown dan /home/dan/.xinitrc
 chgrp dan /home/dan/.xinitrc
-
+echo ""
 echo "Please edit ~/.config/i3 file before starting window manager and reboot computer" 
