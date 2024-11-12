3179af7a28eccd6ce4813ede6872d722f8c113bf
Daniel T <dan.tiganas@gmail.com>
2024-11-12 23:42:51 +0200

    removed unused apps

--- post-config-i3.sh
+++ post-config-i3.sh
@@ -3,11 +3,12 @@
 pkg install xorg
 pkg install i3 i3status i3lock dmenu
 pkg install utouch-kmod xf86-input-evdev
+pkg install rofi xfce4-terminal nitrogen gnome-screenshot qemu-guest-agent mate-backgrounds pavucontrol volumeicon
 
-
-echo "Configuring startup scripts"
+echo "Configuring startup scripts. Make sure VGA driver is selected in Qemu"
 echo "dbus_enable=\"YES\"" >> /etc/rc.conf
 echo "hald_enable=\"YES\"" >> /etc/rc.conf
+echo "qemu_guest_agent_enable=\"YES\"" >> /etc/rc.conf
 
 echo "Enabling mouse in Qemu VirtualMachine"
 echo 'utouch_load="YES"' >> /boot/loader.conf
@@ -20,11 +21,10 @@ mkdir -p /home/dan/.config/i3
 mkdir -p /root/.config/i3
 cp i3/config /home/dan/.config/i3
 cp i3/config /root/.config/i3
-cp i3/conky-i3bar.sh /home/dan./config/i3
-cp i3/conky-i3bar.sh /root/.config/i3
 chown -R dan /home/dan/.config
 chgrp -R dan /home/dan/.config
 chown dan /home/dan/.xinitrc
 chgrp dan /home/dan/.xinitrc
 echo ""
-echo "Please edit ~/.config/i3 file before starting window manager and reboot computer" 
+echo "Please reboot computer, then login as user, startx and run nitrogen to setup wallpaper" 
+echo "Select preferences and add folder /usr/local/share/backgrounds/mate"
