ee481e5cb54d529d051efcf04c2e143d053b42c5
Daniel T <dan.tiganas@gmail.com>
2024-11-12 11:34:36 +0200

    adding I3 config

---
+++ post-config-i3.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+
+pkg install xorg
+pkg install i3 i3status i3lock dmenu
+
+echo "Configuring startup scripts"
+echo "dbus_enable=\"YES\"" >> /etc/rc.conf
+echo "hald_enable=\"YES\"" >> /etc/rc.conf
+
+echo "configuring local variables"
+echo "exec /usr/local/bin/i3" > /home/dan/.xinitrc
+echo "exec /usr/local/bin/i3" > /root/.xinitrc
+mkdir -p /home/dan/.config/i3
+mkdir -p /root/.config/i3
+cp /usr/local/etc/i3/config /home/dan/.config/i3
+cp /usr/local/etc/i3/config /root/.config/i3
+chown dan -R /home/dan/.config
+chgrp dan -R /home/dan/.config
+chown dan /home/dan/.xinitrc
+chgrp dan /home/dan/.xinitrc
+
+echo "Please edit ~/.config/i3 file before starting window manager and reboot computer" 
